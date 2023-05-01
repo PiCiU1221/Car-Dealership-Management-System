27bad4a7302539ea1b70518e609a03257a7aa0b3
PiCiU1221 <96005164+PiCiU1221@users.noreply.github.com>
2023-05-01 15:31:25 +0200

    New vehicle type - Motorcycles and lots of improvements
     - Added motorcycles as a new vehicle type to choose from the menu.
    - <iomanip> library added to improve the displaying methods and to remove LOTS of repetitive hard to read code,
    - A new abstract class, Vehicle was added for other classes to use such as Car or Motorcycle,
    - Main Dealership object is now a static with a new static variable to check for the chosen type in the menu
    - Divided the main functions.cpp file to smaller ones in the menu_functions folder, because it was too hard to navigate and read from,
    - Each class now has itsr own header and cpp file as it should from the start,
    - Updated and added new screenshots to represent the actual status of the program,
    - Many smaller improvements and fixes,

--- main.cpp
+++ main.cpp
@@ -20,29 +20,37 @@ void menu(Dealership& dealership) {
 		cout << "    Second Term Project\n";
 		cout << "    Piotr Pietrusewicz 53828 120A\n";
 		cout << "    Theme: Car Dealearship\n";
-		cout << "================================================================\n\n";
-		cout << "  1 ] Randomize owners and listings\n";
-		cout << "  2 ] Add an owner\n";
-		cout << "  3 ] Remove an owner\n";
+		// Line of 68 '=' characters to make it look better
+		cout << setfill('=') << setw(68) << "=\n\n";
+		if (dealership.getChosenVehicleType() == CAR) {
+			cout << "  1 ] Randomize owners and listings                _.-.___|__\n";
+			cout << "  2 ] Add an owner                                |  _      _`-.\n";
+			cout << "  3 ] Remove an owner                             '-(_)----(_)--`\n";
+		}
+		else if (dealership.getChosenVehicleType() == MOTORCYCLE) {
+			cout << "  1 ] Randomize owners and listings                    ,\n";
+			cout << "  2 ] Add an owner                                  .-/c-.,::\n";
+			cout << "  3 ] Remove an owner                               (_)'==(_)\n";
+		}
 		cout << "  4 ] Display owners\n";
-		cout << "  5 ] Add a new listing\n";
-		cout << "  6 ] Edit a listing\n";
+		cout << "  5 ] Add a new listing                        99 ] Change current\n";
+		cout << "  6 ] Edit a listing                                vehicle type\n";
 		cout << "  7 ] Delete a listing\n";
 		cout << "  8 ] Display listings\n";
 		cout << "  9 ] Search through listings\n";
 		cout << " 10 ] Confirm sale\n";
 		cout << " 11 ] Display amount of owners and listings\n\n";
-		cout << " 12 ] Write to a file\n";
-		cout << " 13 ] Write from a file\n\n";
+		cout << " 12 ] Save to a file\n";
+		cout << " 13 ] Load from a file\n\n";
 		cout << "  0 ] Exit\n\n";
 
 		cin.clear();
 		cin >> choice;
 
-		if (!(choice >= 0 && choice <= 13)) {
+		if (!(choice >= 0 && (choice <= 13 || choice == 99))) {
 			displayIncorrectNumberMessage();
 		}
-	} while (!(choice >= 0 && choice <= 13));
+	} while (!(choice >= 0 && (choice <= 13 || choice == 99)));
 
 	switch (choice) {
 	case 0:
@@ -82,10 +90,14 @@ void menu(Dealership& dealership) {
 		usageOfAnOverloadedOperator(dealership);
 		break;
 	case 12:
-		writingToAFile(dealership);
+		saveToAFile(dealership);
 		break;
 	case 13:
-		enteringDataFromAFile(dealership);
+		loadDataFromAFile(dealership);
+		break;
+	case 99:
+		dealership.setTypeCounterAddOne();
+		dealership.setChosenVehicleType(vehicleTypes[dealership.getTypeCounter() % 2]);
 		break;
 	default:
 		displayIncorrectNumberMessage();
@@ -101,7 +113,7 @@ int main() {
 	srand((unsigned int)time(nullptr));
 
 	// Create an object dealership of a type Dealership to use in the menu function
-	Dealership dealership;
+	static Dealership dealership;
 
 	while (true) {
 		menu(dealership);
